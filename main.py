d2d8499f7dcfe03118300a5f03ca3076c73e1c64
the-shivers <rossbrian120@gmail.com>
2023-06-29 02:01:57 +0000

    More tests working.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+#from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import sqlite3
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -8,4 +9,23 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/nice-text")
 async def nice_text(request: Request):
     return templates.TemplateResponse("nice_text.html", {"request": request})
+
+@app.get("/data")
+async def get_data():
+    # Create/connect to the SQLite database
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Execute a SELECT query
+    cursor.execute("SELECT * FROM my_table")
+    rows = cursor.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    return rows
+
+@app.get("/testywesty")
+async def testywesty(request: Request):
+    return templates.TemplateResponse("testywesty.html", {"request": request})
 
