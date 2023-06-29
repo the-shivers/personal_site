9157d16a240b590283c84c9ff0de2e346c40a2fe
the-shivers <rossbrian120@gmail.com>
2023-06-29 01:02:33 +0000

    Big changes heads up lol

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+@app.get("/nice-text")
+async def nice_text(request: Request):
+    return templates.TemplateResponse("nice_text.html", {"request": request})
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
