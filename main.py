c0fcccd1440a509448915a1872647bdb4d5bb693
the_shivers <rossbrian120@gmail.com>
2023-06-29 23:49:30 -0500

    a

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 #from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 
 app = FastAPI()
