51002ea354535dc30ed55fd5f7f1cddbd9479c3f
the-shivers <rossbrian120@gmail.com>
2023-07-07 02:40:42 +0000

    test

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="/root/personal_site/static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+class MyStaticFiles(StaticFiles):
+    async def get_response(self, path, scope):
+        response = await super().get_response(path, scope)
+        
+        # Add custom headers here
+        response.headers['Cache-Control'] = 'public, max-age=31536000'
+        return response
+
+app.mount("/media", MyStaticFiles(directory="/root/personal_site/media"), name="media")
+
 def split_for_superscript(chord):
    match = re.search(r'(\d|min|Maj|sus)', chord)
    if match:
@@ -17,101 +27,17 @@ def split_for_superscript(chord):
        return chord[:index], chord[index:]
    else:
        return chord, ''
-
-@app.get("/nice-text")
-async def nice_text(request: Request):
-    return templates.TemplateResponse("nice_text.html", {"request": request})
-
-@app.get("/data")
-async def get_data(
-    fret_max: int = 3,
-    fret_stretch: int = 3,
-    tuning_name: str = 'high_g',
-    mutes: str = 'false',
-    limit: int = 1,
-    disallowed_roots: str = '',
-    disallowed_types: str = 'added,jazz_variations_2,jazz_variations_1,altered,extended'
-):
-    conn = sqlite3.connect('chords.db')
-    conn.row_factory = sqlite3.Row   # This allows dictionaries to be returned from the fetch methods. 
-    conn.set_trace_callback(print)
-    cursor = conn.cursor()
-
-    # my_query = f"""
-    #     SELECT *
-    #     FROM strums
-    #     WHERE chord_id <> ''
-    #         AND fret_max <= :fret_max
-    #         AND fret_stretch <= :fret_stretch
-    #         AND tuning_name = :tuning_name
-    #         AND mute_count <= :mutes
-    #         AND chord_type NOT IN (:format_disallowed_types)
-    #         AND root_note NOT IN (:format_disallowed_roots)
-    #     ORDER BY RANDOM() 
-    #     LIMIT :limit;
-    # """
-
-    def build_dict(input_string, base_key):
-        print('in build_dict, inptu string is', input_string)
-        items = input_string.split(',')
-        result_dict = {(f'{base_key}{i}'):(item.replace('s', '#') if len(item) <= 2 else item) for i, item in enumerate(items) }
-        print('in build_dict, results dict', result_dict)
-        return result_dict
-    
-    format_disallowed_roots = build_dict(disallowed_roots, 'root')
-    format_disallowed_types = build_dict(disallowed_types, 'type') 
-
-    placeholders_roots = ', '.join(':' + str(i) for i in format_disallowed_roots.keys())
-    placeholders_types = ', '.join(':' + str(i) for i in format_disallowed_types.keys())
-
-    my_query = f"""
-        SELECT *
-        FROM strums
-        WHERE chord_id <> ''
-            AND fret_max <= :fret_max
-            AND fret_stretch <= :fret_stretch
-            AND tuning_name = :tuning_name
-            AND mute_count <= :mutes
-            AND chord_cat NOT IN ({placeholders_types})
-            AND root_note NOT IN ({placeholders_roots})
-        ORDER BY RANDOM() 
-        LIMIT :limit;
-    """
-
-    mutes_param = 4 if mutes == 'false' else 3
-    my_params = {
-        'mutes': mutes_param,
-        'fret_max': fret_max,
-        'fret_stretch': fret_stretch,
-        'tuning_name': tuning_name,
-        'limit': limit
-    }
-    my_params = {**my_params, **format_disallowed_roots, **format_disallowed_types}
-    print('my params', my_params)
-    cursor.execute(my_query, my_params)
-    print('executed')
-    rows = cursor.fetchall()
-    print('fetched')
-    conn.close()
-    return rows
-
-@app.get("/testywesty")
-async def testywesty(request: Request):
-    return templates.TemplateResponse("testywesty.html", {"request": request})
-
-# @app.get("/chord/{chord_slug}", response_class=HTMLResponse)
-# async def chord(request: Request, chord_slug: str):
-#     return templates.TemplateResponse("chord.html", {"request": request, "chord_slug": chord_slug})
+   
+def split_chord(chord: str) -> tuple:
+    match = re.match(r'([A-Ga-g][s]?)(.*)', chord)
+    if match:
+        return match.group(1), match.group(2)
+    else:
+        return '', ''
 
 @app.get("/uke/{tuning_name}/{chord_abbrv}", response_class=HTMLResponse)
 async def chord(request: Request, chord_abbrv: str, tuning_name: str, id: int = -1):
     try:
-        def split_chord(chord: str) -> tuple:
-            match = re.match(r'([A-Ga-g][s]?)(.*)', chord)
-            if match:
-                return match.group(1), match.group(2)
-            else:
-                return '', ''
         root_note, chord_abbrv = split_chord(chord_abbrv)
         conn = sqlite3.connect('chords.db')
         conn.row_factory = sqlite3.Row
@@ -127,7 +53,6 @@ async def chord(request: Request, chord_abbrv: str, tuning_name: str, id: int = 
         chord_types_data = cursor.fetchall()
         if chord_data is None or tunings_data is None or roots_data is None or chord_types_data is None:
             raise HTTPException(status_code=404, detail="Chord not found")
-
         return templates.TemplateResponse("chord.html", {
             "request": request, 
             "id": id,
@@ -138,10 +63,8 @@ async def chord(request: Request, chord_abbrv: str, tuning_name: str, id: int = 
             "roots_data": roots_data,
             "chord_types_data": chord_types_data
         })
-
     except Exception as error:
         print(f"An error occurred: {error}")
-
     finally:
         if conn:
             conn.close()
@@ -157,20 +80,16 @@ async def root(request: Request, tuning_name: str):
         chord_types = cursor.fetchall()
         cursor.execute('SELECT * FROM notes ORDER BY id')
         roots = cursor.fetchall() 
-
         if chord_types is None or roots is None:
             raise HTTPException(status_code=404, detail="Query returned nothing, probably.")
-
         return templates.TemplateResponse("chords.html", {
             "request": request, 
             "chord_types": chord_types,
             "roots": roots,
             "tuning_name": tuning_name
         })
-    
     except Exception as error:
         print(f"An error occurred: {error}")
-
     finally:
         if conn:
             conn.close()
@@ -184,7 +103,6 @@ async def root(request: Request):
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM tunings ORDER BY id")
         tunings = cursor.fetchall()
-
         if tunings is None:
             raise HTTPException(status_code=404, detail="Query returned nothing, probably.")
 
@@ -192,10 +110,8 @@ async def root(request: Request):
             "request": request, 
             "tunings": tunings
         })
-    
     except Exception as error:
         print(f"An error occurred: {error}")
-
     finally:
         if conn:
             conn.close()
@@ -209,15 +125,11 @@ async def root(request: Request):
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM tunings ORDER BY id")
         tunings = cursor.fetchall()
-
         if tunings is None:
             raise HTTPException(status_code=404, detail="Query returned nothing, probably.")
-
         return "This is my homepage. If you want content, try: https://shivers.dev/uke"
-    
     except Exception as error:
         print(f"An error occurred: {error}")
-
     finally:
         if conn:
             conn.close()
